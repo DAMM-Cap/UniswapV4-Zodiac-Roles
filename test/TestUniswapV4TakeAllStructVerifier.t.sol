c8e0845a75c49d4932dae822b2b3a19e0baa21eb
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2025-05-14 20:32:47 -0300

    test: UniswapV4SettleAllStructVerifier

--- test/TestUniswapV4TakeAllStructVerifier.t.sol
+++ test/TestUniswapV4TakeAllStructVerifier.t.sol
@@ -15,10 +15,10 @@ contract TestUniswapV4TakeAllStructVerifier is Test {
         verifier = new UniswapV4TakeAllStructVerifier();
     }
 
-    function test_takeall_struct_verifier(Currency currency0, Currency currency1, uint256 minAmount) public {
+    function test_takeall_struct_verifier_currency0(Currency currency0, Currency currency1, uint256 minAmount) public {
         // Encode the data as expected by the verifier
         bytes memory data = abi.encode(currency0, minAmount);
-         
+
         // Generate extraData from both currencies
         bytes6 t0 = Lib.tokenHeader(currency0);
         bytes6 t1 = Lib.tokenHeader(currency1);
@@ -38,7 +38,7 @@ contract TestUniswapV4TakeAllStructVerifier is Test {
     function test_takeall_struct_verifier_currency1(Currency currency0, Currency currency1, uint256 minAmount) public {
         // Encode the data but with currency1 instead of currency0
         bytes memory data = abi.encode(currency1, minAmount);
-        
+
         // Generate extraData from both currencies
         bytes6 t0 = Lib.tokenHeader(currency0);
         bytes6 t1 = Lib.tokenHeader(currency1);
@@ -56,26 +56,26 @@ contract TestUniswapV4TakeAllStructVerifier is Test {
     }
 
     function test_takeall_struct_verifier_invalid_currency(
-        Currency currency0, 
-        Currency currency1, 
-        Currency invalidCurrency, 
+        Currency currency0,
+        Currency currency1,
+        Currency invalidCurrency,
         uint256 minAmount
     ) public {
         // Assume the invalid currency is different from both currency0 and currency1
         vm.assume(Currency.unwrap(invalidCurrency) != Currency.unwrap(currency0));
         vm.assume(Currency.unwrap(invalidCurrency) != Currency.unwrap(currency1));
-        
+
         // Further ensure the token headers are different for a robust test
         bytes6 invalidHeader = Lib.tokenHeader(invalidCurrency);
         bytes6 t0 = Lib.tokenHeader(currency0);
         bytes6 t1 = Lib.tokenHeader(currency1);
-        
+
         vm.assume(invalidHeader != t0);
         vm.assume(invalidHeader != t1);
 
         // Encode the data with the invalid currency
         bytes memory data = abi.encode(invalidCurrency, minAmount);
-        
+
         // Generate extraData from the valid currencies
         uint96 packed = (uint96(uint48(bytes6(t0))) << 48) | uint96(uint48(bytes6(t1)));
         bytes12 extraData = bytes12(packed);
