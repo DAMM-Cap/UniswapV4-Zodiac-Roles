938744623c4ec1461137203cfbeec9ae372e367e
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2025-05-17 17:24:00 -0300

    add: optimzer runs for low call cost

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 source .env
 
 # Run the script with verification
-forge script script/DeployVerifiers.s.sol:DeployVerifiers --via-ir --rpc-url $ARBI_RPC_URL --broadcast --verify --etherscan-api-key $ARBISCAN_KEY
+forge script script/DeployVerifiers.s.sol:DeployVerifiers --via-ir --rpc-url $ARBI_RPC_URL --broadcast --verify --etherscan-api-key $ARBISCAN_KEY --optimizer-runs 1000000
